e7b6146b7e3659f9bfcd20e33f9f674268b48b64
Juan Casimiro <juan.casimiro@technologyfromsage.com>
2026-08-22 14:41:02 +0100

    chore: add JUA-48 Spring Boot scaffold

---
+++ src/main/java/com/juancasimiro/spring_mcp_gateway/SpringMcpGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.juancasimiro.spring_mcp_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcpGatewayApplication.class, args);
+	}
+
+}
